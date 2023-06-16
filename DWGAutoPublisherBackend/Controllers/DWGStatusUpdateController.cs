8bfa5d75ac789823e1cce09bc81d27dacb41a51a
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-06-17 00:26:45 +0200

    Potential bug fix

--- DWGAutoPublisherBackend/Controllers/DWGStatusUpdateController.cs
+++ DWGAutoPublisherBackend/Controllers/DWGStatusUpdateController.cs
@@ -3,6 +3,7 @@ using DWGAutoPublisherBackend.Model;
 using DWGAutoPublisherBackend.Model.DrawingHandler;
 using DWGAutoPublisherBackend.Model.DrawingsFromFrontEnd;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -31,6 +32,34 @@ namespace DWGAutoPublisherBackend.Controllers
             if (foundFile == null) return;
 
             foundFile.Status = dwg.Status;
+
+            try
+            {
+                using (var connection = new SqlConnection(Config.SQLConnectionString))
+                {
+                    connection.Open();
+
+                    Console.WriteLine("Successfully connected to the database. LayoutStatusUpdater");
+
+                    string queryString = "UPDATE [master].[dbo].[DWGs] " +
+                        "SET [Status] = @Status " +
+                        "WHERE [Prjoect] = @Prjoect " +
+                        "AND [FileName] = @FileName";
+
+                    using (var command = new SqlCommand(queryString, connection))
+                    {
+                        command.Parameters.AddWithValue("@Status", foundFile.Status);
+                        command.Parameters.AddWithValue("@Prjoect", foundFile.Parent.DisplayName);
+                        command.Parameters.AddWithValue("@FileName", foundFile.FileName);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was an error: " + e.Message);
+            }
         }
     }
 }
